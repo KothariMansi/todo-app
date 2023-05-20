30b71b3689b9d406eea5ac0b20b742c5551863f2
mansi <mansikothari46@gmail.com>
2023-05-20 05:22:38 -0700

    file path improvement

--- udemy_python/GUI.py
+++ udemy_python/GUI.py
@@ -19,7 +19,7 @@ label = sg.Text("Type in a To-Do ")
 input_box = sg.InputText(tooltip="Enter Todo", key='todo')
 
 add_button = sg.Button(key="Add", size=2, image_source='add.png', mouseover_colors='Purple')
-list_box = sg.Listbox(values=functions.get_todos('files/todoss.txt'), key='todos',
+list_box = sg.Listbox(values=functions.get_todos('todoss.txt'), key='todos',
                       enable_events=True, size=[45, 10])
 
 edit_button = sg.Button("Edit")
@@ -37,24 +37,24 @@ window = sg.Window("My To-Do App",
 
 while True:
     event, values = window.read()
-    print(event,values)
+    print(event, values)
     window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
 
     match event:
         case "Add":
-            todos = functions.get_todos('files/todoss.txt')
+            todos = functions.get_todos('todoss.txt')
             new_value = values['todo']+'\n'
             todos.append(new_value)
-            functions.write_todo(todos, 'files/todoss.txt')
+            functions.write_todo(todos, 'todoss.txt')
             window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case 'Complete':
             try:
-                todos = functions.get_todos('files/todoss.txt')
+                todos = functions.get_todos('todoss.txt')
                 todo_to_complete = values['todos'][0]
                 todos.remove(todo_to_complete)
-                functions.write_todo(todos,'files/todoss.txt')
+                functions.write_todo(todos,'todoss.txt')
                 window['todos'].update(values=todos)
                 window['todo'].update(value='')
             except IndexError:
@@ -63,10 +63,10 @@ while True:
             try:
                 todo_to_edit = values['todos'][0]
                 new_todo = values['todo']
-                todos = functions.get_todos('files/todoss.txt')
+                todos = functions.get_todos('todoss.txt')
                 index = todos.index(todo_to_edit)
                 todos[index] = new_todo + '\n'
-                functions.write_todo(todos,'files/todoss.txt')
+                functions.write_todo(todos,'todoss.txt')
                 window['todos'].update(values=todos)
             except IndexError:
                 sg.popup("Please Select an Item First")
--- udemy_python/functions.py
+++ udemy_python/functions.py
@@ -1,21 +1,23 @@
 import os
 
-if not os.path.exists('files/todoss.txt'):
-    with open('files/todoss.txt', 'w') as file:
-        pass
 
+#FILEPATH = 'C:/Users/PC/Desktop/python/udemy_python/files/todoss.txt'
 FILEPATH = 'files/todoss.txt'
+def create_file(FILEPATH):
+    if not os.path.exists(FILEPATH):
+        with open(FILEPATH, 'w') as file:
+            pass
 
 def get_todos(file_path=FILEPATH):
     """Read a text file and return the
        list of todo item."""
-    with open(file_path,'r') as file_local:   #option 2
+    with open(file_path, 'r') as file_local:   #option 2
         todos_local = file_local.readlines()                 #Benefit: Don't need to close file
         return todos_local                                        #Option 2 is recommended over option 1
 
 def write_todo(todos_args,file_path=FILEPATH):
     """ Write the to-do item list in the text file."""
-    with open(file_path,'w') as file:
+    with open(file_path, 'w') as file:
         file.writelines(todos_args)
 
 #print('I am outside!')
