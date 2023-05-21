9acb53af535352d50d68758eae6f000270b80305
mansi <mansikothari46@gmail.com>
2023-05-21 01:33:21 -0700

    first web app(todo app) completed

--- udemy_python/web.py
+++ udemy_python/web.py
@@ -1,18 +1,32 @@
 import streamlit as st
-from functions import get_todos,create_file
+from functions import get_todos,create_file,write_todo
+
 
 FILEPATH = "udemy_python/files/todoss.txt"
-
+#FILEPATH = "files/todoss.txt"
 create_file(FILEPATH)
-#todos = get_todos('udemy_python/files/todoss.txt')
-#todos = get_todos('todoss.txt')
+
 todos = get_todos(FILEPATH)
+
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    write_todo(todos, FILEPATH)
+    st.session_state["new_todo"] = ""
+
 
 st.title("My ToDo App")
 st.subheader("This is My Todo App")
 st.write("This app is for you to help record of things to do.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    Checkbox = st.checkbox(todo, key=todo)
+    if Checkbox:
+        todos.pop(index)
+        write_todo(todos, FILEPATH)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add new todo....", key='x')
+
+st.text_input('todo_add', label_visibility='hidden', placeholder="Add new todo....",
+              on_change=add_todo, key='new_todo')
